4e14d3ba23c078a73ba717efacb2d71e75b01fc9
MadCortez <ilyacortezz@gmail.com>
2025-01-11 22:40:04 +0300

    Rename

--- src/main/java/com/madcortezz/notes/repositories/INotesRepository.java
+++ src/main/java/com/madcortezz/notes/repositories/INotesRepository.java
@@ -3,5 +3,5 @@ package com.madcortezz.notes.repositories;
 import com.madcortezz.notes.models.NoteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface INoteRepository extends JpaRepository<NoteModel, Long> {
+public interface INotesRepository extends JpaRepository<NoteModel, Long> {
 }
