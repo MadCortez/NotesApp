5db3cdb44a344dfb6e4d5ecee7a186296c318583
MadCortez <ilyacortezz@gmail.com>
2025-01-11 22:44:39 +0300

    Add showing all notes Add creating note Add deleting note

--- src/main/java/com/madcortezz/notes/controllers/NotesController.java
+++ src/main/java/com/madcortezz/notes/controllers/NotesController.java
@@ -1,21 +1,30 @@
 package com.madcortezz.notes.controllers;
 
-import com.madcortezz.notes.repositories.INoteRepository;
+import com.madcortezz.notes.models.NoteModel;
+import com.madcortezz.notes.repositories.INotesRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class NotesController {
 
-    private final INoteRepository noteRepository;
+    private final INotesRepository notesRepository;
 
-    public NotesController(INoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
+    public NotesController(INotesRepository notesRepository) {
+        this.notesRepository = notesRepository;
     }
 
     @GetMapping
     public String index(Model model) {
+        List<NoteModel> notes = notesRepository.findAll();
+        model.addAttribute("notes", notes);
+
         return "index";
     }
 
@@ -27,5 +36,18 @@ public class NotesController {
     @GetMapping("/privacy")
     public String privacy() {
         return "privacy";
+    }
+
+    @PostMapping("/notes/create")
+    public String createNote(@ModelAttribute NoteModel note) {
+
+        notesRepository.save(note);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteNote(@PathVariable("id") Long id) {
+        notesRepository.deleteById(id);
+        return "redirect:/";
     }
 }
